a485d0568ddec23d265980804821f7485c094a97
AlexeyJKoshkin <AlexeyJKoshkin@gmail.com>
2022-05-15 15:43:56 +0300

    Impruve animation. Clean code. remove unused

--- TweenAnimation/BezierPathCurveAnimation.cs
+++ TweenAnimation/BezierPathCurveAnimation.cs
@@ -7,33 +7,29 @@ namespace GameKit.TweenAnimation
 {
     public class BezierPathCurveAnimation : MonoBehaviour
     {
-        [SerializeField] private List<AnimateableObject> _animatableObjects;
-        [SerializeField] private float _homeMovingTime = 4;
+        [SerializeField] private List<Transform> _animatableObjects;
+        [SerializeField, SerializeReference] private CircleBezierCurve _bezierCurve;
 
-        [SerializeField, SerializeReference] private IBezierCurve _bezierCurve;
-
-        [SerializeField] private Direction _direction = Direction.Back;
-
-
-        [ButtonGroup("Circle")]
-        void MoveToCirclePosition()
+        public Tween MoveToStartCurvePosition(float duration)
         {
-            var delta = 1f / _animatableObjects.Count;
+            var      delta    = 1f / _animatableObjects.Count;
+            Sequence sequence = DOTween.Sequence();
             for (int i = 0; i < _animatableObjects.Count; i++)
             {
-                _animatableObjects[i].MoveTo(_bezierCurve.GetPointAt(delta * i), 3);
+                var tween = _animatableObjects[i].DOMove(_bezierCurve.GetPointAt(delta * i), duration);
+                sequence.Join(tween);
             }
+
+            return sequence;
         }
 
-
-        [ButtonGroup("Circle")]
-        void MovingCircle()
+        public Tween MoveByCurvePosition(float duration, Direction direction)
         {
-            var   startEnd = GetStartEndValue();
+            var   startEnd = GetStartEndValue(direction);
             float counter  = startEnd.start;
             var   delta    = 1f / _animatableObjects.Count;
             Debug.LogError(_bezierCurve.GetPointAt(0));
-            DOTween.To(() => counter, (s) =>
+            return DOTween.To(() => counter, (s) =>
                                       {
                                           counter = s;
                                           for (int i = 0; i < _animatableObjects.Count; i++)
@@ -41,36 +37,19 @@ namespace GameKit.TweenAnimation
                                               var val = delta * i + counter;
                                               if (val > 1)
                                                   val -= 1;
-                                              _animatableObjects[i].SetPos(_bezierCurve.GetPointAt(val));
+                                              _animatableObjects[i].position =_bezierCurve.GetPointAt(val);
                                           }
-                                      }, startEnd.end, _homeMovingTime).SetEase(Ease.Linear);
+                                      }, startEnd.end, duration).SetEase(Ease.Linear);
         }
 
 
-        private (float start, float end) GetStartEndValue()
+        private (float start, float end) GetStartEndValue(Direction direction)
         {
-            if (_direction == Direction.Forward) return (0, 1);
+            if (direction == Direction.Forward) return (0, 1);
             else return (1, 0);
         }
 
-
-        [ButtonGroup("Random")]
-        void SetRandom()
-        {
-            _animatableObjects.ForEach(e =>
-                                       {
-                                           Vector3 pos = new Vector3(Random.Range(-19, 70), Random.Range(-30, 30), 0);
-                                           e.SetPos(pos);
-                                       });
-        }
-
-        [ButtonGroup("Random")]
-        void MoveHomePosition()
-        {
-            _animatableObjects.ForEach(e => e.MoveHoMe(_homeMovingTime));
-        }
-
-        enum Direction
+      public  enum Direction
         {
             Forward,
             Back
--- TweenAnimation/IBezierCurve.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace GameKit.TweenAnimation {
-    public interface IBezierCurve
-    {
-        Vector3 GetPointAt(float delta);
-    }
-}
