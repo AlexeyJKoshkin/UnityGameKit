7212ebe07cc1b6c8da964c24797ca291e0d6292f
AlexeyJKoshkin <AlexeyJKoshkin@gmail.com>
2022-09-07 11:27:34 +0300

    Update AbstractGameEditorLauncher.cs

--- Editor/EditorDIContainerExtension/CustomGameEditor/AbstractGameEditorLauncher.cs
+++ Editor/EditorDIContainerExtension/CustomGameEditor/AbstractGameEditorLauncher.cs
@@ -1,4 +1,4 @@
-﻿using GameKit.EditorContext;
+using GameKit.EditorContext;
 using UnityEngine;
 using VContainer;
 
@@ -6,10 +6,10 @@ namespace GameKit.CustomGameEditor
 {
     public abstract class AbstractGameEditorLauncher : ScriptableObject
     {
+        [ReadOnly]
         public string Description;
         public abstract bool IsWork { get; }
         public virtual string EditorName => name;
-
         [SerializeField] private BaseGameEditorInstallers[] _installers;
 #if ODIN_INSPECTOR
         [Sirenix.OdinInspector.Button, Sirenix.OdinInspector.HideIf("IsWork")]
@@ -25,16 +25,16 @@ namespace GameKit.CustomGameEditor
             LunchEditor(resolver);
         }
 
-        protected virtual void Binding(ContainerBuilder diContainer)
+        protected virtual void Binding(IContainerBuilder diContainer)
         {
-            _installers.ForEach(e => e.InstallBindings(diContainer));
+            _installers.ForEach(e => e.Install(diContainer));
         }
 
         protected abstract void LunchEditor(IObjectResolver diContainer);
 
-        protected virtual void PreBinding(ContainerBuilder diContainer) { }
+        protected virtual void PreBinding(IContainerBuilder diContainer) { }
 
-        protected virtual void PostBinding(ContainerBuilder diContainer) { }
+        protected virtual void PostBinding(IContainerBuilder diContainer) { }
 
         protected virtual void PreLunch() { }
 
