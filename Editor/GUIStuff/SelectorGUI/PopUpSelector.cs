d90707572b1465e0ecb95f807f25b68f6340ecdd
AlexeyJKoshkin <AlexeyJKoshkin@gmail.com>
2022-09-09 17:31:15 +0300

    fix odin selector callback

--- Editor/GUIStuff/SelectorGUI/PopUpSelector.cs
+++ Editor/GUIStuff/SelectorGUI/PopUpSelector.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Sirenix.OdinInspector.Editor;
 using UnityEditor;
 using UnityEngine;
 
@@ -15,93 +16,93 @@ namespace GameKit.Editor
         public PopUpSelector(params T[] values) : base(values, null, 0) { }
 
         public PopUpSelector(IEnumerable<T> data = null,
-                             int defaultIndex = -1,
-                             ContentMaker<T> guicontentHelper = null) : base(data,
-                                                                             guicontentHelper,
-                                                                             defaultIndex) { }
+                             int defaultIndex = -1, ContentMaker<T> guicontentHelper = null) : base(data,
+                                                                                                    guicontentHelper,
+                                                                                                    defaultIndex) { }
 
-        public PopUpSelector(IEnumerable<T> data,
-                             ContentMaker<T> guicontentHelper,
+        public PopUpSelector(IEnumerable<T> data, ContentMaker<T> guicontentHelper,
                              int defaultIndex = -1) : base(data, guicontentHelper, defaultIndex) { }
+
 #if ODIN_INSPECTOR
-
         private int _odinDirty = -1;
-
-        protected override int DrawSelectionGUI(int index, GUIContent label, float labelwidth = 70)
+        
+        protected override int SelecttionGUI(int index, GUIContent label, float labelwidth = 70)
         {
             using (new EditorGUILayout.HorizontalScope())
             {
-                if (label != GUIContent.none) GUILayout.Label(label, GUILayout.Width(labelwidth));
-
+                if (label != GUIContent.none)
+                    GUILayout.Label(label, GUILayout.Width(labelwidth));
                 if (GUILayout.Button(_valueBox.GetContent(CurrentValue), EditorStyles.popup))
                 {
-                    var genericSelector = new GenericSelector<T>(this);
-                    var pos = Event.current.mousePosition;
-                    Sirenix.OdinInspector.Editor.OdinEditorWindow odinEditorWindow = genericSelector.ShowInPopup(pos);
+                   
+                    var              customOdinDrawer = new GenericSelector<T>(this);
+                    var              pos              = Event.current.mousePosition;
+                    OdinEditorWindow odinEditorWindow = customOdinDrawer.ShowInPopup(pos);
 
-                    odinEditorWindow.OnClose += () =>
                     {
-                        var item = (T) genericSelector.SelectionTree.Selection.SelectedValue;
-                        _odinDirty = _valueBox.IndexOf(item);
-                        this.SetCurrentByIndex(_odinDirty);
-                    };
-                    odinEditorWindow.OnClose += genericSelector.SelectionTree.Selection.ConfirmSelection;
+                        odinEditorWindow.OnClose += () =>
+                                                    {
+                                                        var item = (T) customOdinDrawer.SelectionTree.Selection.SelectedValue;
+                                                        _odinDirty = _valueBox.IndexOf(item); 
+                                                        SetCurrentByIndex(_odinDirty);
+                                                    };
+                    }
+
+                    odinEditorWindow.OnClose += customOdinDrawer.SelectionTree.Selection.ConfirmSelection;
                     if (Application.platform == RuntimePlatform.LinuxEditor) GUIUtility.ExitGUI();
                 }
             }
 
             if (_odinDirty > -1)
             {
-                index = _odinDirty;
+                index      = _odinDirty;
                 _odinDirty = -1;
             }
 
             return index;
         }
-
 #else
         protected override int SelecttionGUI(int index, GUIContent label, float labelwidth = 70)
         {
-            var info = PrepareValues(index);
+            var selectionData = GetSelectionData(index);
 
-            using (new GUILayout.HorizontalScope())
-            {
-                if (label != GUIContent.none) GUILayout.Label(label, GUILayout.Width(labelwidth));
-                var res = EditorGUILayout.Popup(index, info.names, GUILayout.ExpandHeight(Expand));
-                return PostCheck(info.hasEmpty, res);
-            }
+            GUILayout.BeginHorizontal();
+
+            if (label != GUIContent.none)
+                GUILayout.Label(label, GUILayout.Width(labelwidth));
+            var res = EditorGUILayout.Popup(index, selectionData.names, GUILayout.ExpandHeight(Expand));
+            GUILayout.EndHorizontal();
+            return PostCheck(selectionData.hasEmpty, res);
         }
-
 #endif
 
-        protected override int DrawSelectionGUI(int index, Rect rect, GUIContent label)
+        protected override int SelecttionGUI(int index, Rect rect, GUIContent label)
         {
-            var info = PrepareValues(index);
-            var res = EditorGUI.Popup(rect, new GUIContent(label), index, info.names, EditorStyles.popup);
-            return PostCheck(info.hasEmpty, res);
+            var selectionData = GetSelectionData(index);
+            var res = EditorGUI.Popup(rect, new GUIContent(label), index, selectionData.names, EditorStyles.popup);
+            return PostCheck(selectionData.hasEmpty, res);
         }
 
-        (GUIContent[] names, int index, bool hasEmpty) PrepareValues(int index)
+        private int PostCheck(bool hasEmpty, int res)
         {
-            var names = _valueBox.NameContent;
-            var hasEmpty = index == -1;
+            return hasEmpty  //если надо добавить пустой объект
+                ? (res == 0) // и выбран пустой . т.к. выбран 0 (самый первый)
+                    ? -1     // то должны вернуть -1, чтобы в следущий раз опять добавить пустоту
+                    : res - 1 // если выбран не пустой, то вернем res--, т.к. 0 элемент фейковый
+                : res;
+        }
 
-            if (hasEmpty)
+        private (GUIContent[] names, int index, bool hasEmpty) GetSelectionData(int index)
+        {
+            var names      = _valueBox.NameContent;
+            var needSelect = index == -1;
+            if (needSelect)
             {
                 index = 0;
                 ArrayUtility.Insert(ref names, 0, new GUIContent(EmptyName));
             }
 
-            return (names, index, hasEmpty);
-        }
-
-        int PostCheck(bool hasEmpty, int resultIndex)
-        {
-            return hasEmpty
-                ? resultIndex == 0
-                    ? -1
-                    : resultIndex - 1
-                : resultIndex;
+            return (names, index, needSelect);
         }
 
         protected override string DefaultGetName(T obj, int n)
